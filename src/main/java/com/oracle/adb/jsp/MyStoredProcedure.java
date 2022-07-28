90439f10c00f9d07590fe939037fe08149f7bba8
LLEFEVRE <loic.lefevre@gmail.com>
2022-07-28 11:20:38 +0200

    Initial commit

--- src/main/java/com/oracle/adb/jsp/MyStoredProcedure.java
+++ src/main/java/com/oracle/adb/jsp/MyStoredProcedure.java
@@ -1,22 +1,20 @@
 package com.oracle.adb.jsp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.SQLException;
+
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MyStoredProcedure {
-	public static String main(String name) throws SQLException {
-		try (Connection c = DriverManager.getConnection("jdbc:default:connection:")) {
+	public static String main(String name) /*throws SQLException*/ {
+/*		try (Connection c = DriverManager.getConnection("jdbc:default:connection:")) {
 			try(Statement s = c.createStatement()) {
 				try( ResultSet r = s.executeQuery("SELECT current_date FROM dual")) {
 					if(r.next()) {
 						return String.format( "Hello %s, today is %s!", name == null ? "World" : name, r.getString(1));
 					}
 				}
-			}
-			return String.format( "Hello %s!", name == null ? "World" : name);
-		}
+			}*/
+		return String.format("Hello %s!", name == null ? "World" : name);
+		//}
 	}
 }
