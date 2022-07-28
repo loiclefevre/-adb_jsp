016f2a78fd28f4d732ff34a7edfdd9e3e4df7a8a
LLEFEVRE <loic.lefevre@gmail.com>
2022-07-28 15:07:52 +0200

    New greetings message

--- src/main/java/com/oracle/adb/jsp/MyStoredProcedure.java
+++ src/main/java/com/oracle/adb/jsp/MyStoredProcedure.java
@@ -2,6 +2,9 @@ package com.oracle.adb.jsp;
 
 import java.sql.SQLException;
 
+/**
+ * Java Stored Procedure!
+ */
 public class MyStoredProcedure {
 	public static String hello(String name) throws SQLException {
 		return String.format("Hello %s!", name == null ? "World" : name);
